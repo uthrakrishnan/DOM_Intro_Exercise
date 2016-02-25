5e4fabf993d735f96d2f7510b6d34969984d8cdd
Uthra Krishnan <uakrishnan@gmail.com>
2016-02-25 15:23:55 -0800

    Initial commit with first two homework points done

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 console.log("Javascript is alive!");
+
+//Change greeting from Hello there to "Hello, World!""
+var hello = document.getElementById('greeting');
+
+    hello.innerHTML="Hello, World!";
+//Set background color of each <li> to yellow.
+
+var listElements = document.getElementsByTagName('li');
+    for( var i=0; i<listElements.length; i++){
+    listElements[i].style.backgroundColor = "yellow";
+}
+
+//Create an image tag and append to the #greeting div.
+var picture = document.createElement('img');
+
+    picture.src = "http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif";
+    hello.append(picture);
+
+
